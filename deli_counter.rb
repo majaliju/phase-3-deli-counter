38b9826d885fc5d961a6fa5ee81ca3ce0adee38c
majaliju <majalijunyc@gmail.com>
2022-11-06 18:59:57 -0500

    commit tree f319ffb28a9d087765d3f6f37847e48cdb71b2c5

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  elsif array.length >= 1
+    line_list = array.map.each_with_index{|person, index| "#{index+1}. #{person}"}.join(" ")
+    puts "The line is currently: #{line_list}"
+  end
+end
+
+def take_a_number(array, new_customer)
+  array.push(new_customer)
+  puts "Welcome, #{new_customer}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+  current_customer = array.shift
+  puts "Currently serving #{current_customer}."
+  end
+end
